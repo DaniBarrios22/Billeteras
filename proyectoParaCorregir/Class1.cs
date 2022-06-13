fe007041ef5b46e3263e2103dad433a1483f39e7
Daniela Barrios <dani_bj1@hotmail.com>
2022-06-13 09:42:23 -0300

    Agregar archivos de proyecto.

---
+++ proyectoParaCorregir/Class1.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Billeteras
+{
+    internal class Billetera
+    {
+        public int BilletesDe10 { get; set; }
+        public int BilletesDe20 { get; set; }
+        public int BilletesDe50 { get; set; }
+        public int BilletesDe100 { get; set; }
+        public int BilletesDe200 { get; set; }
+        public int BilletesDe500 { get; set; }
+        public int BilletesDe1000 { get; set; }
+
+        public decimal Total()
+        {
+            return BilletesDe10*10 + BilletesDe20*20 + BilletesDe50*50 + BilletesDe100*100 + BilletesDe200*200
+                + BilletesDe500*500 + BilletesDe1000*1000;
+        }
+        public Billetera Combinar(Billetera otraBilletera)
+        {
+            Billetera nuevaBilletera = new Billetera();
+
+            int sumaBilletesDe10 = BilletesDe10 + otraBilletera.BilletesDe10;
+            int sumaBilletesDe20 = BilletesDe20 + otraBilletera.BilletesDe20;
+            int sumaBilletesDe50 = BilletesDe50 + otraBilletera.BilletesDe50;
+            int sumaBilletesDe100 = BilletesDe100 + otraBilletera.BilletesDe100;
+            int sumaBilletesDe200 = BilletesDe200 + otraBilletera.BilletesDe200;
+            int sumaBilletesDe500 = BilletesDe500 + otraBilletera.BilletesDe500;
+            int sumaBilletesDe1000 = BilletesDe1000 + otraBilletera.BilletesDe1000;
+
+            nuevaBilletera.BilletesDe10 = sumaBilletesDe10;
+            nuevaBilletera.BilletesDe20 = sumaBilletesDe20;
+            nuevaBilletera.BilletesDe50 = sumaBilletesDe50;
+            nuevaBilletera.BilletesDe100 = sumaBilletesDe100;
+            nuevaBilletera.BilletesDe200 = sumaBilletesDe200;
+            nuevaBilletera.BilletesDe500 = sumaBilletesDe500;
+            nuevaBilletera.BilletesDe1000 = sumaBilletesDe1000;
+
+            return nuevaBilletera ; 
+        }
+    }
+}
---
+++ proyectoParaCorregir/Program.cs
@@ -0,0 +1,29 @@
+﻿using Billeteras;
+
+var Billetera1 = new Billetera();
+
+Billetera1.BilletesDe10 = 7;
+Billetera1.BilletesDe20 = 6;
+Billetera1.BilletesDe50 = 5;
+Billetera1.BilletesDe100 = 4;
+Billetera1.BilletesDe200 = 3;
+Billetera1.BilletesDe500 = 2;
+Billetera1.BilletesDe1000 = 1;
+
+var Billetera2 = new Billetera();
+
+Billetera2.BilletesDe10 = 1;
+Billetera2.BilletesDe20 = 6;
+Billetera2.BilletesDe50 = 1;
+Billetera2.BilletesDe100 = 4;
+Billetera2.BilletesDe200 = 1;
+Billetera2.BilletesDe500 = 2;
+Billetera2.BilletesDe1000 = 1;
+
+Console.WriteLine(Billetera1.Total());
+Console.WriteLine(Billetera2.Total());
+
+
+Billetera Billetera3 = Billetera2.Combinar(Billetera1);
+
+Console.WriteLine(Billetera3.Total()); 
